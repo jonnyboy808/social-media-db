7a2512a0fcce7187c37df517ca82af15149db95d
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-05-20 15:43:49 -0700

    created models for reaction, thoughts, and user. Created link to connection.js

---
+++ models/Thought.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+const reactionSchema = require('./Reaction');
+
+// Schema to create the thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema
+  .virtual('reactionCount')
+  // Getter
+  .get(function () {
+    return this.reactions.length;
+  })
+
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create the username model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/]
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  })
+  // Setter to set the username
+  .set(function (virtual) {
+    const username = virtual;
+    this.set({ username });
+  });
+
+
+const User = model('User', userSchema);
+
+module.exports = User;
